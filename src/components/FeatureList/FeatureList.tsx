15b3e1c6e67dfac2dc873131e441b8858911f0c6
Varun Santhanam <talkto@vsanthanam.com>
2021-11-30 20:04:13 -0800

    Support Dark Mode

--- src/components/FeatureList/FeatureList.tsx
+++ src/components/FeatureList/FeatureList.tsx
@@ -9,13 +9,18 @@ class FeatureList extends React.Component {
         return(
             <div className="feature-list-section-container">
                 <div className="feature-list-section-content">
-                    <div className="row">
-                        <Feature name="Open Any Song, Anywhere" description="Convusic supports many of the most popular services including Apple Music, Spotify, Tidal, and YouTube Music" icon="artist"/>
-                        <Feature name="Simple & Instant" description="Enable Convusic and forget about it. Any music streaming link will automatically open in your app of choice. No interaction required." icon="puzzle"/>
+                    <div className="feature-grid">
+                        <div className="row">
+                            <Feature name="Open Any Song, Anywhere" description="Convusic supports many of the most popular services including Apple Music, Spotify, Tidal, and YouTube Music" icon="artist"/>
+                            <Feature name="Simple & Instant" description="Enable Convusic and forget about it. Any music streaming link will automatically open in your app of choice. No interaction required." icon="puzzle"/>
+                        </div>
+                        <div className="row">
+                            <Feature name="Private & Secure" description="Convusic's client apps and browser extensions feature no advertising, use no third-party trackers, and collect no user identifiable data of any kind." icon="shield"/>
+                            <Feature name="Cross Platform" description="Convusic works on iPhone, iPad, and Macintosh, and supports iCloud syncing and Handoff for a seamless, integrated experience." icon="artist"/>
+                        </div>
                     </div>
-                    <div className="row">
-                        <Feature name="Private & Secure" description="Convusic's client apps and browser extensions feature no advertising, use no third-party trackers, and collect no user identifiable data of any kind." icon="shield"/>
-                        <Feature name="Cross Platform" description="Convusic works on iPhone, iPad, and Macintosh, and supports iCloud syncing and Handoff for a seamless, integrated experience." icon="artist"/>
+                    <div className="inspiration">
+                        <p className="inspiration">Inspired by <a href="https://itunes.apple.com/app/id1585734696">Amplosion</a> from <a href="https://www.twitter.com/ChristianSelig">@ChristianSelig</a></p>
                     </div>
                 </div>
             </div>
