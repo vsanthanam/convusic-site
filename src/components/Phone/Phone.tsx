b3f9aeb3e8131ae6e195110df4badbd00ae1c055
Varun Santhanam <talkto@vsanthanam.com>
2021-11-30 22:43:48 -0800

    Use dark screenshot in dark mode

--- src/components/Phone/Phone.tsx
+++ src/components/Phone/Phone.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Phone.scss';
 import LightPhone from './images/LightPhone.png';
+import DarkPhone from './images/DarkPhone.png';
 class Phone extends React.Component {
 
     render() {
         return(
             <div className="phone-container">
-                <img className="phone-bitmap" src={LightPhone} />
+                <picture>
+                    <source srcSet={DarkPhone} media="(prefers-color-scheme: dark)" />
+                    <img className="phone-bitmap" src={LightPhone} alt= "moving car" />
+                </picture>
             </div>
         )
     }
