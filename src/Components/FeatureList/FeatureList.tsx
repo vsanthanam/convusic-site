f5e6bee8e773faf86491ea6b922c038fafbc5841
Varun Santhanam <talkto@vsanthanam.com>
2021-12-02 17:09:49 -0800

    Use unified scss colors in Features.scss

--- src/Components/FeatureList/FeatureList.tsx
+++ src/Components/FeatureList/FeatureList.tsx
@@ -9,11 +9,11 @@ class FeatureList extends React.Component {
             <div className="feature-list-section-container">
                 <div className="feature-list-section-content">
                     <div className="feature-grid">
-                        <div className="row">
+                        <div className="feature-row">
                             <Feature name="Open Any Song, Anywhere" description="Convusic supports many of the most popular services including Apple Music, Spotify, Tidal, and YouTube Music" icon="artist"/>
                             <Feature name="Simple & Instant" description="Enable Convusic and forget about it. Any music streaming link will automatically open in your app of choice. No interaction required." icon="puzzle"/>
                         </div>
-                        <div className="row">
+                        <div className="feature-row">
                             <Feature name="Private & Secure" description="Convusic's client apps and browser extensions feature no advertising, use no third-party trackers, and collect no user identifiable data of any kind." icon="shield"/>
                             <Feature name="Cross Platform" description="Convusic works on iPhone, iPad, and Macintosh, and supports iCloud syncing and Handoff for a seamless, integrated experience." icon="connected-apps"/>
                         </div>
