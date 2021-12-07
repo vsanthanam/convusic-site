eb4ce0e9b9d1437f9ad61b46e4881b2410829414
Varun Santhanam <talkto@vsanthanam.com>
2021-12-07 10:21:30 -0800

    Update FAQ

--- src/PressKit/PressKit.tsx
+++ src/PressKit/PressKit.tsx
@@ -21,6 +21,63 @@ class Question extends React.Component<QuestionProps> {
 
 }
 
+class FeatureTable extends React.Component {
+
+    render(): JSX.Element {
+        return(
+            <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Song</th>
+                        <th>Album</th>
+                        <th>Artist</th>
+                        <th>Playlist</th>
+                        <th>Podcast</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th className="service-name">Apple Music</th>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                    </tr>
+                    <tr>
+                        <th className="service-name">Spotify</th>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                    </tr>
+                    <tr>
+                        <th className="service-name">Tidal</th>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                    </tr>
+                    <tr>
+                        <th className="service-name">YouTube Music</th>
+                        <td>✅</td>
+                        <td>✅</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                        <td>❌</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        );
+    }
+
+}
+
 class PressKit extends React.Component {
 
     render(): JSX.Element {
@@ -47,56 +104,10 @@ class PressKit extends React.Component {
                     <Question title="Does it slow down my web browser?" answer={<p>Nope! Convusic doesn't even have the permissions to run on websites that aren't music streaming services. It requires no background processes and consumes no additional battery life, activating only when you visit the website belonging to one of the four supported services.</p>} />
                     <Question title="Can I choose which music app to use manually each time?" answer={<p>Yes! You can choose to open the music link in the Convusic app using a button in the extension menu, and manually select your service of choice each time. This can be useful if you use multiple streaming services or are installing Convusic on a shared family computer.</p>} />
                     <Question title="Do I need to buy Convusic multiple times for all my devices?" answer={<p>Nope! A single purchase on the app store includes both the iOS and the macOS versions, and can be installed on every device you own.</p>} />
-                    <Question title="What services and media types are supported?" answer={<p>Convusic support most music content from Apple Music, Spotify, Tidal, and YouTube Music. New media types and services are added over time. Podcasts are not supported.</p>} />
-                    <div className="table-container">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Song</th>
-                                    <th>Album</th>
-                                    <th>Artist</th>
-                                    <th>Playlist</th>
-                                    <th>Podcast</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th className="service-name">Apple Music</th>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                </tr>
-                                <tr>
-                                    <th className="service-name">Spotify</th>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                </tr>
-                                <tr>
-                                    <th className="service-name">Tidal</th>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                </tr>
-                                <tr>
-                                    <th className="service-name">YouTube Music</th>
-                                    <td>✅</td>
-                                    <td>✅</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                    <td>❌</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <Question title="What services and media types are supported?" answer={<p>Convusic support most music content from Apple Music, Spotify, Tidal, and YouTube Music. New media types and services are added over time. Podcasts and playlists are not currently itasfasfsupported.</p>} />
+                    <FeatureTable />
                     <Question title="I use a different music streaming service. Can you add support for it?" answer={<p>Maybe! If the service has a public facing catalog search API, I'm happy to investigate adding support. Send me an email with the details.</p>} />
+                    <Question title="I'd like a promo code to test Convusic or access to TestFlight. Can you add me?" answer={<p>Maybe! Send me an email with the details and I'll see what I can do.</p>} />
                 </div>
             </div>
         )
