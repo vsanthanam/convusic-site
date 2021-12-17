5aeab13a8f21407f63abc2adde229c933be01304
Varun Santhanam <talkto@vsanthanam.com>
2021-12-16 18:04:19 -0800

    Add icons for other services

--- src/Components/Banner/Components/Explanation/Explanation.tsx
+++ src/Components/Banner/Components/Explanation/Explanation.tsx
@@ -7,18 +7,67 @@ import AppStoreDark from './images/AppStore-Dark.svg';
 import AppleMusicMobile from './images/AppleMusicMobile.svg';
 import LightSpotifyMobile from './images/LightSpotifyMobile.svg';
 import DarkSpotifyMobile from './images/DarkSpotifyMobile.svg';
+import DarkPandora from './images/DarkPandora.png';
+import LightPandora from './images/LightPandora.svg';
+import DarkTidal from './images/DarkTidal.svg';
+import LightTidal from './images/LightTidal.svg';
+
+enum MusicIdentifier {
+    AppleMusic,
+    Spotify,
+    Pandora,
+    Tidal
+}
 
 class Explanation extends React.Component {
 
     render(): JSX.Element {
-        return(
-            <div className="explanation-container">
-                <div className="explanation-content">
-                    <div className="artwork-container">
+
+        const services = [MusicIdentifier.AppleMusic, MusicIdentifier.Spotify, MusicIdentifier.Pandora, MusicIdentifier.Tidal];
+
+        const firstIdentifier = services[Math.floor(Math.random() * services.length)]
+
+        const remainingServices = services.filter(service => service !== firstIdentifier);
+
+        const secondIdentifier = remainingServices[Math.floor(Math.random() * remainingServices.length)];
+
+        const elementForService = function (service: MusicIdentifier) {
+            switch (service) {
+                case MusicIdentifier.AppleMusic:
+                    return(
+                        <picture>
+                            <img className="app-icon" src={AppleMusicMobile} alt="Apple Music" />
+                        </picture>
+                    )
+                case MusicIdentifier.Spotify:
+                    return(
                         <picture>
                             <source srcSet={DarkSpotifyMobile} media="(prefers-color-scheme: dark)" />
                             <img className="app-icon" src={LightSpotifyMobile} alt="Spotify"/>
                         </picture>
+                    )
+                case MusicIdentifier.Pandora:
+                    return(
+                        <picture>
+                            <source srcSet={DarkPandora} media="(prefers-color-scheme: dark)" />
+                            <img className="app-icon" src={LightPandora} alt="Pandora" />
+                        </picture>
+                    )
+                case MusicIdentifier.Tidal:
+                    return(
+                        <picture>
+                            <source srcSet={DarkTidal} media="(prefers-color-scheme: dark)" />
+                            <img className="app-icon" src={LightTidal} alt="Tidal" />
+                        </picture>
+                    )
+            }
+        }
+
+        return(
+            <div className="explanation-container">
+                <div className="explanation-content">
+                    <div className="artwork-container">
+                        {elementForService(firstIdentifier)}
                         <div className="arrow">
                             <svg role="presentation" width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
@@ -33,9 +82,7 @@ class Explanation extends React.Component {
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
                             </svg>
                         </div>
-                        <picture>
-                            <img className="app-icon" src={AppleMusicMobile} alt="Apple Music" />
-                        </picture>
+                        {elementForService(secondIdentifier)}
                     </div>
                     <div className="download-container">
                         <div className="download-content">
