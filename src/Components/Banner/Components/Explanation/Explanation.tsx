2f67d91d282b6648c1319d650234d8106894a82b
Varun Santhanam <talkto@vsanthanam.com>
2021-12-16 18:51:33 -0800

    Update icon choices on banner component

--- src/Components/Banner/Components/Explanation/Explanation.tsx
+++ src/Components/Banner/Components/Explanation/Explanation.tsx
@@ -23,13 +23,9 @@ class Explanation extends React.Component {
 
     render(): JSX.Element {
 
-        const services = [MusicIdentifier.AppleMusic, MusicIdentifier.Spotify, MusicIdentifier.Pandora, MusicIdentifier.Tidal];
+        const services = [MusicIdentifier.AppleMusic, MusicIdentifier.AppleMusic, MusicIdentifier.Pandora, MusicIdentifier.Tidal];
 
-        const firstIdentifier = services[Math.floor(Math.random() * services.length)]
-
-        const remainingServices = services.filter(service => service !== firstIdentifier);
-
-        const secondIdentifier = remainingServices[Math.floor(Math.random() * remainingServices.length)];
+        const identifier = services[Math.floor(Math.random() * services.length)]
 
         const elementForService = function (service: MusicIdentifier) {
             switch (service) {
@@ -67,7 +63,7 @@ class Explanation extends React.Component {
             <div className="explanation-container">
                 <div className="explanation-content">
                     <div className="artwork-container">
-                        {elementForService(firstIdentifier)}
+                        {elementForService(MusicIdentifier.Spotify)}
                         <div className="arrow">
                             <svg role="presentation" width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
@@ -82,7 +78,7 @@ class Explanation extends React.Component {
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
                             </svg>
                         </div>
-                        {elementForService(secondIdentifier)}
+                        {elementForService(identifier)}
                     </div>
                     <div className="download-container">
                         <div className="download-content">
