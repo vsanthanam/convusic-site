b7d7d3ac719e8aa28ebc0c31b627e0b7f0a3cccf
Varun Santhanam <talkto@vsanthanam.com>
2021-12-21 21:39:47 -0800

    amazon (#1)
    * Add Amazon Logo
    
    * Add Amazon Music to press kit page

--- src/Components/Banner/Components/Explanation/Explanation.tsx
+++ src/Components/Banner/Components/Explanation/Explanation.tsx
@@ -4,9 +4,10 @@ import DesktopAppIcon from './images/AppIcon-Desktop.svg';
 import MobileAppIcon from './images/AppIcon-Mobile.svg';
 import AppStoreLight from './images/AppStore-Light.svg';
 import AppStoreDark from './images/AppStore-Dark.svg';
-import AppleMusicMobile from './images/AppleMusicMobile.svg';
-import LightSpotifyMobile from './images/LightSpotifyMobile.svg';
-import DarkSpotifyMobile from './images/DarkSpotifyMobile.svg';
+import AppleMusic from './images/AppleMusic.svg';
+import AmazonMusic from './images/AmazonMusic.svg';
+import LightSpotify from './images/LightSpotify.svg';
+import DarkSpotify from './images/DarkSpotify.svg';
 import DarkPandora from './images/DarkPandora.png';
 import LightPandora from './images/LightPandora.svg';
 import DarkTidal from './images/DarkTidal.svg';
@@ -15,6 +16,7 @@ import DarkYouTube from './images/DarkYouTube.svg';
 import LightYouTube from './images/LightYouTube.svg';
 
 enum MusicIdentifier {
+    AmazonMusic,
     AppleMusic,
     Spotify,
     Pandora,
@@ -27,29 +29,33 @@ class Explanation extends React.Component {
     render(): JSX.Element {
 
         const services = [
+            MusicIdentifier.AmazonMusic,
             MusicIdentifier.AppleMusic,
+            MusicIdentifier.Spotify,
             MusicIdentifier.Pandora,
-            MusicIdentifier.AppleMusic,
             MusicIdentifier.Tidal,
-            MusicIdentifier.AppleMusic,
             MusicIdentifier.YouTube
         ];
 
-        const identifier = services[Math.floor(Math.random() * services.length)]
+        const originService = services[Math.floor(Math.random() * services.length)];
+
+        const filteredServices = services.filter(service => service !== originService);
+
+        const targetService = filteredServices[Math.floor(Math.random() * filteredServices.length)];
 
         const elementForService = function (service: MusicIdentifier) {
             switch (service) {
                 case MusicIdentifier.AppleMusic:
                     return(
                         <picture>
-                            <img className="app-icon" src={AppleMusicMobile} alt="Apple Music" />
+                            <img className="app-icon" src={AppleMusic} alt="Apple Music" />
                         </picture>
                     )
                 case MusicIdentifier.Spotify:
                     return(
                         <picture>
-                            <source srcSet={DarkSpotifyMobile} media="(prefers-color-scheme: dark)" />
-                            <img className="app-icon" src={LightSpotifyMobile} alt="Spotify"/>
+                            <source srcSet={DarkSpotify} media="(prefers-color-scheme: dark)" />
+                            <img className="app-icon" src={LightSpotify} alt="Spotify"/>
                         </picture>
                     )
                 case MusicIdentifier.Pandora:
@@ -73,6 +79,12 @@ class Explanation extends React.Component {
                             <img className="app-icon" src={LightYouTube} alt="YouTube Music" />
                         </picture>
                     )
+                case MusicIdentifier.AmazonMusic:
+                    return(
+                        <picture>
+                            <img className="app-icon" src={AmazonMusic} alt="Amazon Music" />
+                        </picture>
+                    )
             }
         }
 
@@ -80,7 +92,7 @@ class Explanation extends React.Component {
             <div className="explanation-container">
                 <div className="explanation-content">
                     <div className="artwork-container">
-                        {elementForService(MusicIdentifier.Spotify)}
+                        {elementForService(originService)}
                         <div className="arrow">
                             <svg role="presentation" width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
@@ -95,7 +107,7 @@ class Explanation extends React.Component {
                                 <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM23.7071 8.70711C24.0976 8.31658 24.0976 7.68342 23.7071 7.29289L17.3431 0.928932C16.9526 0.538408 16.3195 0.538408 15.9289 0.928932C15.5384 1.31946 15.5384 1.95262 15.9289 2.34315L21.5858 8L15.9289 13.6569C15.5384 14.0474 15.5384 14.6805 15.9289 15.0711C16.3195 15.4616 16.9526 15.4616 17.3431 15.0711L23.7071 8.70711ZM1 9H23V7H1V9Z" fill="#000000"/>
                             </svg>
                         </div>
-                        {elementForService(identifier)}
+                        {elementForService(targetService)}
                     </div>
                     <div className="download-container">
                         <div className="download-content">
